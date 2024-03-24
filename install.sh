757680e5f65ff18d1e40cb39dad31172f59c03b5
sarmadkhan96 <sarmadkhan1996@hotmail.com>
2024-03-24 03:53:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ sudo apt install docker.io -y
 # Start Docker service using systemd
 sudo systemctl start docker
 
-#Pull the Apache Server image
+# Pull the Apache Server image
 sudo docker pull httpd
 
 # Docker subnet
 sudo docker network create --subnet="172.168.10.0/30" my_network
 
-#Port forward on local to EC2 on Port 80
+# Port forward on local to EC2 on Port 80
 sudo docker run -d --name=myserver -p 80:80 httpd
 
 # Connect the custom subnet to the docker container
